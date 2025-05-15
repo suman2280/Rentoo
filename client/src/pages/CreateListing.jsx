14d84a277c84e3e5e36f8023d5440b20f8493572
Suman Sardar <sudarshansardar22@gmail.com>
2025-05-15 18:45:49 +0530

    Changed some symbols

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -499,7 +499,7 @@ const CreateListing = () => {
                 required
               />
               <p>Now, set your PRICE</p>
-              <span>$</span>
+              <span>₹</span>
               <input
                 type="number"
                 placeholder="100"
